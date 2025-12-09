0f4c4e0b8f98a300d364c4472adf16867e6adf59
Stepane B <binon.stepane@gmail.com>
2025-12-09 19:30:47 +1000

    Import and setup TIM1

--- ESC_TOOLKIT_TARGET_STM32G431RB/main.cpp
+++ ESC_TOOLKIT_TARGET_STM32G431RB/main.cpp
@@ -18,12 +18,12 @@
 
 ADC_HandleTypeDef hadc1;
 ADC_HandleTypeDef hadc2;
-// TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim1;
 
 void SystemClock_Config(void);
 void GPIO_Init(void);
 void ADC1_Init(void);
-// static void MX_TIM1_Init(void);
+static void MX_TIM1_Init(void);
 
 enum class Sector { 
     S0, S1, S2, S3, S4, S5
@@ -315,6 +315,72 @@ void ADC1_Init(void)
     
     HAL_ADCEx_Calibration_Start(&hadc1, ADC_SINGLE_ENDED);
 }
+
+// static void MX_TIM1_Init(void)
+// {
+//   TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+//   TIM_MasterConfigTypeDef sMasterConfig = {0};
+//   TIM_OC_InitTypeDef sConfigOC = {0};
+//   TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+//   htim1.Instance = TIM1;
+//   htim1.Init.Prescaler = 0;
+//   htim1.Init.CounterMode = TIM_COUNTERMODE_CENTERALIGNED1;
+//   htim1.Init.Period = 0xffff;
+//   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+//   htim1.Init.RepetitionCounter = 0;
+//   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+//   if (HAL_TIM_Base_Init(&htim1) != HAL_OK) {
+//     Error_Handler();
+//   }
+//   sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+//   if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK) {
+//     Error_Handler();
+//   }
+//   if (HAL_TIM_PWM_Init(&htim1) != HAL_OK) {
+//     Error_Handler();
+//   }
+//   sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+//   sMasterConfig.MasterOutputTrigger2 = TIM_TRGO2_RESET;
+//   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+//   if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK) {
+//     Error_Handler();
+//   }
+//   sConfigOC.OCMode = TIM_OCMODE_PWM1;
+//   sConfigOC.Pulse = 0;
+//   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+//   sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+//   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+//   sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+//   sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+//   if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK) {
+//     Error_Handler();
+//   }
+//   if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK) {
+//     Error_Handler();
+//   }
+//   if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_3) != HAL_OK) {
+//     Error_Handler();
+//   }
+//   sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_ENABLE;
+//   sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_ENABLE;
+//   sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+//   sBreakDeadTimeConfig.DeadTime = 0x32;
+//   sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+//   sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+//   sBreakDeadTimeConfig.BreakFilter = 0;
+//   sBreakDeadTimeConfig.BreakAFMode = TIM_BREAK_AFMODE_INPUT;
+//   sBreakDeadTimeConfig.Break2State = TIM_BREAK2_DISABLE;
+//   sBreakDeadTimeConfig.Break2Polarity = TIM_BREAK2POLARITY_HIGH;
+//   sBreakDeadTimeConfig.Break2Filter = 0;
+//   sBreakDeadTimeConfig.Break2AFMode = TIM_BREAK_AFMODE_INPUT;
+//   sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_ENABLE;
+//   if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK) {
+//     Error_Handler();
+//   }
+  
+//   HAL_TIM_MspPostInit(&htim1);
+// }
 
 void GPIO_Init(void)
 {
